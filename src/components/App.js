3d39cc285ec998b1962b56be0e0a3174d3cf1422
Takahiro <tkhr_621@docomo.ne.jp>
2021-03-10 00:07:57 +0900

    feat

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,18 @@ import '../App.css';
 import  { UseState } from './UseState'
 import { UseEffect } from './UseEffect'
 import { UseRef } from './UseRef';
+import { Hello } from './ReactMemo';
+import { UseCallback } from './UseCallback';
+
+
 export const App = () =>  {
   return (
     <div className="App">
       <UseState />
       <UseEffect />
       <UseRef />
+      <Hello />
+      <UseCallback />
     </div>
   );
 }
